74e5171d614407f1a1f9bbe5739c257758230e5f
Arik <4ortik@gmail.com>
2014-04-27 19:43:36 +0300

    Initial commit

---
+++ src/main/java/ac/il/technion/twc/TwitterKnowledgeCenter.java
@@ -0,0 +1,53 @@
+package ac.il.technion.twc;
+
+/**
+ * This class is meant to act as a wrapper to test your functionality. You should implement all its methods
+ * and not change any of their signatures. You can also implement an argumentless constructor if you wish.
+ * 
+ * @author Gal Lalouche
+ */
+public class TwitterKnowledgeCenter {
+    /**
+     * Loads the data from an array of lines
+     * 
+     * @param lines An array of lines, each line formatted as <time (dd/MM/yyyy HH:mm:ss)>,<tweet id>[,original tweet]
+     * @throws Exception If for any reason, handling the data failed
+     */
+    public void importData(String[] lines) throws Exception {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+    
+    /**
+     * Loads the index, allowing for queries on the data that was imported using
+     * {@link TwitterKnowledgeCenter#importData(String[])}. setupIndex will be called before any queries can be run
+     * on the system
+     * 
+     * @throws Exception If for any reason, loading the index failed
+     */
+    public void setupIndex() throws Exception {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+    
+    /**
+     * Gets the lifetime of the tweet, in milliseconds.
+     * 
+     * @param tweetId The tweet's identifier
+     * @return A string, counting the number of milliseconds between the tweet's publication and its last retweet
+     * @throws Exception If it is not possible to complete the operation
+     */
+    public String getLifetimeOfTweets(String tweetId) throws Exception {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+    
+    /**
+     * Gets the weekly histogram of all tweet data
+     * 
+     * @return An array of strings, each string in the format of
+     *         ("<number of tweets (including retweets), number of retweets only>"), for example:
+     *         ["100, 10","250,20",...,"587,0"]. The 0th index of the array is Sunday.
+     * @throws Exception If it is not possible to complete the operation
+     */
+    public String[] getDailyHistogram() {
+        throw new UnsupportedOperationException("Not implemented");
+    }
+}
