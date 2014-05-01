cca1c8fb40f111bf347c654680dfb5f3eee9c4fd
Arik <4ortik@gmail.com>
2014-05-01 23:27:16 +0300

    some updates

---
+++ src/main/java/ac/il/technion/twc/StoreAbleTweet.java
@@ -0,0 +1,35 @@
+package ac.il.technion.twc;
+
+import java.util.Date;
+
+public class StoreAbleTweet extends Tweet
+{
+	private final long lifeTime;
+
+	public StoreAbleTweet(Tweet tweet, long lifeTime)
+	{
+		super();
+		this.lifeTime = lifeTime;
+		this.id = tweet.getId();
+		this.originalDate = tweet.getOriginalDate();
+		this.isOriginal = tweet.isOriginal();
+		this.originalTweet = tweet.getOriginalTweet();
+	}
+
+	public StoreAbleTweet(String id, Date originalDate, boolean isOriginal,
+			String originalTweet, long lifeTime)
+	{
+		super();
+		this.lifeTime = lifeTime;
+		this.id = id;
+		this.originalDate = originalDate;
+		this.isOriginal = isOriginal;
+		this.originalTweet = originalTweet;
+	}
+
+	public long getLifeTime()
+	{
+		return lifeTime;
+	}
+
+}
--- src/main/java/ac/il/technion/twc/Tweet.java
+++ src/main/java/ac/il/technion/twc/Tweet.java
@@ -1,36 +1,19 @@
 package ac.il.technion.twc;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
 
 public class Tweet
 {
 
-	private final String id;
-	private Date originalDate;
-	private final boolean isOriginal;
-	private String originalTweet;
+	protected String id;
+	protected Date originalDate;
+	protected boolean isOriginal;
+	protected String originalTweet;
 
-	// TODO check if tweet can be declared twice
-	public Tweet(String line)
+	public Tweet()
 	{
 		super();
-		final String[] args = line.split(",");
-		final SimpleDateFormat dateFormat = new SimpleDateFormat(
-				"dd/MM/yyyy hh:mm:ss");
-		try
-		{
-			this.originalDate = dateFormat.parse(args[0]);
-		} catch (final ParseException e)
-		{
-		}
-		this.id = args[1].replace(" ", "");
-		if (args.length == 3)
-			originalTweet = args[2].replace(" ", "");
-		this.isOriginal = args.length != 3;
-
 	}
 
 	@Override
@@ -66,4 +49,5 @@ public class Tweet
 		calendar.setTime(originalDate);
 		return calendar.get(Calendar.DAY_OF_WEEK);
 	}
+
 }
