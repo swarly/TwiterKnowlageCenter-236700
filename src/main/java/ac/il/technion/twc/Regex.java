a254d3c677516d89316428f2d31fd9d6f05ff699
Eyal B <brkeyal@gmail.com>
2014-05-31 21:12:15 +0300

    no message

---
+++ src/main/java/ac/il/technion/twc/Regex.java
@@ -0,0 +1,6 @@
+package ac.il.technion.twc;
+
+public class Regex
+{
+
+}
--- src/main/java/ac/il/technion/twc/impl/tweet/TweetFactory.java
+++ src/main/java/ac/il/technion/twc/impl/tweet/TweetFactory.java
@@ -4,13 +4,13 @@ import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Collection;
 import java.util.Date;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.List;
 
 import org.json.JSONObject;
 
 import ac.il.technion.twc.impl.HashTagImpl;
 import ac.il.technion.twc.impl.IHashTag;
+import ac.il.technion.twc.twitter.Extractor;
 
 import com.google.common.collect.Lists;
 
@@ -30,7 +30,7 @@ public class TweetFactory
 
 	/**
 	 * use for creating tweet from the Json dataHandler
-	 *
+	 * 
 	 * @param jsonObject
 	 * @return
 	 */
@@ -39,7 +39,7 @@ public class TweetFactory
 		return new StringLineCompleteTweet(jsonObject.getString(ITweet.idName), new Date(
 				jsonObject.getLong(ITweet.timeName)), jsonObject.optString(ITweet.originalName).isEmpty(), jsonObject
 				.optString(ITweet.originalName).isEmpty() ? null : jsonObject.optString(ITweet.originalName),
-						jsonObject.getLong(ITweet.liftimeName));
+				jsonObject.getLong(ITweet.liftimeName));
 	}
 
 	public static ITweet newPersistableStringLineTweet(ITweet tweet, long twittLifeTime)
@@ -56,25 +56,29 @@ public class TweetFactory
 	/* Adds all hashtags from 'text' parameter to hashTagsCollection parameter */
 	private static void parseHashTags(Collection<IHashTag> hashTags, String text)
 	{
-		final String patternStr = "(?:\\s|\\A)[##]+([A-Za-z0-9-_]+)";
-		final Pattern pattern = Pattern.compile(patternStr);
-		final Matcher matcher = pattern.matcher(text);
-		String result = "";
-		// Search for Hashtags
-		while (matcher.find())
+		final Extractor extractor = new Extractor();
+
+		final List<String> res = extractor.extractHashtags(text);
+
+		for (final String element : res)
 		{
-			result = matcher.group();
-			result = result.replace(" ", "");
-			final String search = result.replace("#", "");
-			final IHashTag currHashTag = new HashTagImpl(search);
+			final IHashTag currHashTag = new HashTagImpl(element);
 			hashTags.add(currHashTag);
 		}
+
+		/*
+		 * final String patternStr = "(?:\\s|\\A)[##]+([A-Za-z0-9-_]+)"; final Pattern pattern =
+		 * Pattern.compile(patternStr); final Matcher matcher = pattern.matcher(text); String result = ""; // Search for
+		 * Hashtags while (matcher.find()) { result = matcher.group(); result = result.replace(" ", ""); final String
+		 * search = result.replace("#", ""); final IHashTag currHashTag = new HashTagImpl(search);
+		 * hashTags.add(currHashTag); }
+		 */
 
 	}
 
 	/**
 	 * use for importing tweet from Tweeter Json format
-	 *
+	 * 
 	 * @param jsonObject
 	 * @return
 	 * @throws ParseException
